7984c4f0fc2c3ebfa16eb009b97dc6549d81f56b
shefa alhendi <shefa.alhendi@quarksoftware.net>
2026-04-11 23:15:12 +0300

    3

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  base: "/tamrat/",
+  base: "/tamrat_web/",
   plugins: [react(), tailwindcss()],
   assetsInclude: ["**/*.glb", "**/*.gltf"],
   resolve: {
